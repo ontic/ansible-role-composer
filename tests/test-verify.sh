f8479c82a8c103676f3a7d5789e4a159808d1b3b
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 19:05:35 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed version of Composer.
-docker exec --tty ${container_id} env TERM=xterm composer || true
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which composer
+# Verify the installed version.
+docker exec --tty ${container_id} env TERM=xterm composer
